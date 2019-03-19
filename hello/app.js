c0712419b2b783b4f5b12767fd5be632feef8d15
Rick Nunes <sanunes.ricardo@gmail.com>
2019-03-18 23:07:15 -0400

    Added routes

--- hello/app.js
+++ hello/app.js
@@ -1,14 +1,35 @@
 var express = require("express");
 var logger = require("morgan");
+var path = require("path");
 var http = require("http");
 
 var app = express();
 
 app.use(logger("short"));
 
+var publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+
+app.get("/", function(req, res) {
+    res.end("Welcome to my homepage.");
+});
+
+app.get("/about", function(req, res) {
+    res.end("This is the about page.");
+});
+
+app.get("/weather", function(req, res) {
+    res.end("No wheater API yet");
+});
+
+app.use(function(req, res) {
+    res.statusCode = 404;
+    res.end("404");
+});
+
 app.use(function(req, res) {
     res.writeHead(200, {"Content-type": "text/plain"});
-    res.end("Hello World");
+    res.end("No static file");
 });
 
 
