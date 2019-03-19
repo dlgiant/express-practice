daa08601881f7da168d3ee97c7ab859ff0d8b6d7
Rick Nunes <sanunes.ricardo@gmail.com>
2019-03-18 22:53:05 -0400

    With middelware

--- hello/app.js
+++ hello/app.js
@@ -6,25 +6,6 @@ var app = express();
 
 app.use(logger("short"));
 
-app.use(function(req, res, next) {
-    console.log("Request to: "+req.method+" to "+req.url);
-    next();
-});
-
-app.use(function(req, res, next) {
-    var minute = (new Date()).getMinutes();
-    if ((minute%2) === 0) {
-	next();
-    } else {
-	res.statusCode = 403;
-	res.end("Not authorized.");
-    }
-});
-
-app.use(function(req, res) {
-    res.end('Secret info: "If if you can\'t love yourself..."');
-});
-
 app.use(function(req, res) {
     res.writeHead(200, {"Content-type": "text/plain"});
     res.end("Hello World");
