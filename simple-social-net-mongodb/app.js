5fda38ad3b2d3e41d6b3e54d31ac1bc9d9bbf155
Rick Nunes <sanunes.ricardo@gmail.com>
2019-03-20 13:59:11 -0400

    Fixed MongoDB connections

--- simple-social-net-mongodb/app.js
+++ simple-social-net-mongodb/app.js
@@ -11,7 +11,18 @@ var routes = require("./routes");
 var app = express();
 
 // Testing connection to MongoDB
-mongoose.connect("mongobd://localhost:27017/test");
+const mongo = mongoose.connect("mongodb://localhost:27017/test", {
+		useNewUrlParser: true,
+		useFindAndModify: true,
+		useCreateIndex: true,
+		keepAlive: 1,
+		reconnectTries: 10
+})
+mongo.then(() => {
+	console.log("Database is connected!");
+}).catch((err) => {
+	console.log("error:", err);
+});
 
 app.set("port", process.env.PORT || 3000);
 
