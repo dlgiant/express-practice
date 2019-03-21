6c1a68cc4da1cf639868f1b7600ea267226a2b09
Rick Nunes <sanunes.ricardo@gmail.com>
2019-03-21 13:39:18 -0400

    Fixes for local authentication

--- simple-social-net-mongodb/app.js
+++ simple-social-net-mongodb/app.js
@@ -5,8 +5,10 @@ var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 var session = require("cookie-session");
 var flash = require("connect-flash");
-
+var session = require("express-session");
+var passport = require("passport");
 var routes = require("./routes");
+var setUpPassport = require("./setuppassport");
 
 var app = express();
 
@@ -17,12 +19,15 @@ const mongo = mongoose.connect("mongodb://localhost:27017/test", {
 		useCreateIndex: true,
 		keepAlive: 1,
 		reconnectTries: 10
-})
+});
+setUpPassport();
+
 mongo.then(() => {
 	console.log("Database is connected!");
 }).catch((err) => {
 	console.log("error:", err);
 });
+
 
 app.set("port", process.env.PORT || 3000);
 
@@ -32,12 +37,14 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({
-	secret: "Areallylonganddifficulthash",
+	secret: "slay dragons night",
 	resave: true,
 	saveUninitialized: true
 }));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(routes);
 
 app.listen(app.get("port"), function(){
